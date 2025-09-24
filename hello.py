3b9c96acbdaa9d143c56aa55f2987422547279f8
Ahmad Danish <danishh.1511@gmail.com>
2025-09-24 14:11:46 +0800

    Added hello.py program

---
+++ hello.py
@@ -0,0 +1,7 @@
+# hello.py
+# A simple Python program
+def greet(name):
+ return f"Hello, {name}! Welcome to GitHub and Python."
+if __name__ == "__main__":
+ user = input("Enter your name: ")
+ print(greet(user))
